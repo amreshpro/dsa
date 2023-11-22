92ff24e7d10c86ad32327b6f6634259da60b73e6
Amresh Maurya <amresh.terminal@gmail.com>
2023-11-22 21:54:01 +0530

    pattern1

---
+++ Pattern/p1.java
@@ -0,0 +1,11 @@
+public class p1{
+  public static void main(String[] args) {
+    //loop1
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+    System.err.print("*");
+      }
+      System.err.println();
+    }
+  }
+}
