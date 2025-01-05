263ce85e3828a4b42458f871fe5acd57da390d01
Amresh Maurya <amresh.terminal@gmail.com>
2025-01-05 21:30:52 +0530

    hashing min max freq

--- src/pro/hashing/MinAndMaxFrequency.java
+++ src/pro/hashing/MinAndMaxFrequency.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class MinAndMaxFrequency {
     static public Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         int n;
         System.out.println("Enter number of elements: ");
@@ -15,10 +16,10 @@ public class MinAndMaxFrequency {
             arr[i] = sc.nextInt();
         }
 
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
 
         int minFreqCount = map.get(arr[0]);
@@ -26,19 +27,19 @@ public class MinAndMaxFrequency {
         int max = 0;
         int maxFreqCount = map.get(arr[0]);
         for (int i = 0; i < n; i++) {
-          if(  map.get(arr[i])   > maxFreqCount){
-              maxFreqCount = map.get(arr[i]);
-              max = i ;
-          }
-          if(map.get(arr[i]) < minFreqCount){
-              minFreqCount = map.get(arr[i]);
-              min = i ;
-          }
+            if (map.get(arr[i]) > maxFreqCount) {
+                maxFreqCount = map.get(arr[i]);
+                max = i;
+            }
+            if (map.get(arr[i]) < minFreqCount) {
+                minFreqCount = map.get(arr[i]);
+                min = i;
+            }
         }
 
 
-        System.out.println("Max: " + arr[max]+
-                " Min: " + arr[min]);
+        System.out.println("Max Frequent: " + arr[max] + " Min Frequent: " + arr[min]);
+        System.out.println("Max Count: " + map.get(arr[max]) + ", Min Count: " + map.get(arr[min]));
     }
 
 
