528c6b62f85048e8ba7341ea5511a1210c8c0af5
Amresh Maurya <amresh.terminal@gmail.com>
2025-01-10 20:17:18 +0530

    math: implement sieve

--- src/pro/math/GeneratePrimeToN.java
+++ src/pro/math/GeneratePrimeToN.java
@@ -2,6 +2,7 @@ public class GeneratePrimeToN {
 
     // Sieve of Eratosthenes. 
     public static void main(String[] args) {
-        
+        Integer i = new Integer(10);
+        System.out.println(i.byteValue());
     }
 }
