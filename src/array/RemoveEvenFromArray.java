8dcaf8573c7401d73238c50a6e0a85eb1999af45
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-10 22:34:36 +0530

    remove even from arr

---
+++ src/array/RemoveEvenFromArray.java
@@ -0,0 +1,34 @@
+package array;
+
+public class RemoveEvenFromArray {
+
+    public  static  int[] removeEvenArray(int[] arr){
+        int oddCount = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if((arr[i] & 1) != 0){
+                ++oddCount;
+            }
+        }
+
+
+        int[] res = new int[oddCount];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if((arr[i] & 1) != 0){
+                res[index] = arr[i];
+                ++index;
+            }
+        }
+
+        return  res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5,6,7,8,9,10};
+      int[] ans =  removeEvenArray(arr);
+      for (int i : ans){
+          System.out.print(i+" ");
+      }
+    }
+}
