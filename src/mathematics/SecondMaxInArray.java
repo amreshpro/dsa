48486f9748e36495e3be15ad66801c5b4980fde1
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-12 22:45:22 +0530

    second max array

---
+++ src/mathematics/SecondMaxInArray.java
@@ -0,0 +1,28 @@
+package mathematics;
+
+public class SecondMaxInArray {
+
+public static int secondMax(int[] arr){
+    if(arr.length < 2 ){
+          throw new IllegalArgumentException("Invalid input");
+    }
+    int secondMax = arr[0];
+    int max = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+        if(arr[i] > max){
+            secondMax = max ;
+            max = arr[i];
+        } else if (arr[i] !=max && arr[i] > secondMax) {
+            secondMax = arr[i];
+
+        }
+    }
+
+    return  secondMax;
+}
+    public static void main(String[] args) {
+        int[] arr = { 1,2,3,4,5,6,7};
+     int res =   secondMax(arr);
+        System.out.println(res);
+    }
+}
